d31193ee8936ba42306c0a83861382db1f5bb19e
Naharashu <130159705+Naharashu@users.noreply.github.com>
2025-04-08 21:36:43 +0300

    Update router.js

--- moduls_light/router.js
+++ moduls_light/router.js
@@ -3,8 +3,32 @@ import { Dom } from '/moduls_light/Dom.js'
 
 const dom = new Dom
 
+
 const routers = {
-  '/home': () => dom.query("app").html("<p>Main page</p>"),
-  '/about': () => dom.query("app").html("<p>About page</p>"),
-  '/download': () => dom.query("app").html("<p>Download page</p>")
+    '/': 'index.html',
+    '/home': 'home.html',
+    '/about': 'about.html',
+    '/download': 'download.html',
+    '/404': '404.html'
 }
+
+
+const route = (event) => {
+    event = event || window.event
+    event.preventDefault()
+    window.history.pushState({}, '', event.target.href)
+    handleLocation()
+    }
+
+const handleLocation = async () => {
+    const path = window.location.pathname
+    const route = routers[path] || routers['/404']
+    const html = await fetch(route).then(data => data.text())
+    dom.query('body').html(html)
+}
+
+window.onpopstate = handleLocation
+window.route = route
+
+
+handleLocation()
